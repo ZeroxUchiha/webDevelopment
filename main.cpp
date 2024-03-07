8ea7d6ac839c2c9f2dcae66104966111f005ad1d
ZeroxUchiha <Zeroxyde2003@gmail.com>
2024-03-06 19:28:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <cstdlib>
+
 int main() {
     // Ouvrir toute mes applications
     system("start filezilla.exe");
     system("start \"\" \"C:\\Users\\Pierre-Carl\\Desktop\\Visual Studio Code.lnk\"");
     system("\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\" https://www.youtube.com/watch?v=jfKfPfyJRdk");
     system("\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\" https://chat.openai.com/");
+    system("start GitHubDesktop.exe");
 
     return 0;
 }
