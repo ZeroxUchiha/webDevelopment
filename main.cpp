f4fddb9df83d4c5b02f5c3ab1f8561bb07b7d2cd
ZeroxUchiha <Zeroxyde2003@gmail.com>
2024-03-06 19:35:20 -0500

    asdd github start

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     system("start \"\" \"C:\\Users\\Pierre-Carl\\Desktop\\Visual Studio Code.lnk\"");
     system("\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\" https://www.youtube.com/watch?v=jfKfPfyJRdk");
     system("\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\" https://chat.openai.com/");
-    system("start GitHubDesktop.exe");
+    system("start \"\" \"C:\\Users\\Pierre-Carl\\AppData\\Local\\GitHubDesktop\\GitHubDesktop.exe\"");
 
     return 0;
 }
